af1ccf7ea36f92547b58d79887fd0cbbb866f299
初音过去 <wjlfish@qq.com>
2022-01-24 23:03:25 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -35,14 +35,14 @@
           $t('nav.home')
         }}</router-link>
         <router-link
-          to="/library"
-          :class="{ active: $route.name === 'library' }"
-        >{{ $t('nav.library') }}</router-link
-        >
-        <router-link
           to="/settings"
           :class="{ active: $route.name === 'settings' }"
           >{{ $t('library.userProfileMenu.settings') }}</router-link
+        >
+        <router-link
+          to="/library"
+          :class="{ active: $route.name === 'library' }"
+          >{{ $t('nav.library') }}</router-link
         >
       </div>
       <div class="right-part">
